77a2379362cc5ee4e1a218a338a3eec4aec59b09
Maor Bar <86046742+maor224@users.noreply.github.com>
2024-12-07 17:24:28 +0000

    beautify chat_server code

--- chat_server.py
+++ chat_server.py
@@ -2,51 +2,52 @@ import socket
 import threading
 
 CONNECTION_LIST = []
-ser_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = '0.0.0.0'
 PORT = 9860
+ser_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ser_sock.bind((HOST, PORT))
-# listen
 ser_sock.listen(2)
-print('Chat server started on port : ' + str(PORT))
+
+print(f'Chat server started on port: {PORT}')
 
 
 def accept_client():
     while True:
-        # accept
         cli_sock, cli_add = ser_sock.accept()
         CONNECTION_LIST.append(cli_sock)
-        print('new client is now connected')
-        thread_client = threading.Thread(target=broadcast_usr, args=[cli_sock])
+        print('New client connected')
+        thread_client = threading.Thread(target=broadcast_usr, args=(cli_sock,))
         thread_client.start()
 
 
 def broadcast_usr(cli_sock):
-    while len(CONNECTION_LIST) > 0:
+    while cli_sock in CONNECTION_LIST:
         try:
-            print(CONNECTION_LIST)
             data = cli_sock.recv(1024).decode()
-            print(data)
+            print(f"Received message: {data}")
+
             if data != "stop\n":
-                send(cli_sock, data)
+                send_message(cli_sock, data)
             else:
                 CONNECTION_LIST.remove(cli_sock)
                 cli_sock.close()
-        except Exception as x:
-            print(x)
+                print('Client disconnected')
+        except Exception as e:
+            print(f"Error: {e}")
             break
 
 
-def send(cs_sock, msg):
-    print(CONNECTION_LIST)
+def send_message(cs_sock, msg):
+    print(f"Broadcasting message: {msg}")
     for client in CONNECTION_LIST:
         if client != cs_sock:
-            print(msg)
-            client.send(msg.encode())
-            print("sent")
+            try:
+                client.send(msg.encode())
+                print(f"Message sent to {client}")
+            except Exception as e:
+                print(f"Failed to send message to {client}: {e}")
 
 
 def start():
     thread_ac = threading.Thread(target=accept_client)
     thread_ac.start()
-
